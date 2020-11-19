565bdcc081840dee5bb049d3b03a361a3aeb1dbc
Adityasingh675 <adisingh675@gmail.com>
2020-11-19 17:49:40 +0530

    clockwise and anticlockwise animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:animation_2/brick_anim.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,7 +7,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: BrickPage(),
     );
   }
 }
